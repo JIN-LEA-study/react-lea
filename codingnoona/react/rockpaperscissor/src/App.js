93b971040e2412f78577eb36017ca26ca69b3d53
leaisrevolution <leaisrevolution@gmail.com>
2022-05-15 10:53:43 +0900

    rockpaperscissor UI

--- codingnoona/react/rockpaperscissor/src/App.js
+++ codingnoona/react/rockpaperscissor/src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
+import Box from "./componet/Box";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="main">
+        <Box title="You" />
+        <Box title="Computer" />
+      </div>
+      <div className="main">
+        <button>가위</button>
+        <button>바위</button>
+        <button>보</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
