e94c18e848a67e4e4bb42806be0be7eea55bb982
leaisrevolution <leaisrevolution@gmail.com>
2022-05-15 17:30:31 +0900

    현재 위치 가져오기

--- codingnoona/react/weather-app/src/App.js
+++ codingnoona/react/weather-app/src/App.js
@@ -1,6 +1,20 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // 현재위치정보
+  const getCurrentLocation = () => {
+    // get Current Location JS
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("현재위치", lat, lon);
+    });
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
   return <div>hiii</div>;
 }
 
