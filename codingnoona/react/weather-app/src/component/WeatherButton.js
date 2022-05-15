91328e41b3a8929e8a2ac87d91da18b1b924c79c
leaisrevolution <leaisrevolution@gmail.com>
2022-05-15 19:38:08 +0900

    도시별 날씨 가져오기

--- codingnoona/react/weather-app/src/component/WeatherButton.js
+++ codingnoona/react/weather-app/src/component/WeatherButton.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ cities }) => {
+const WeatherButton = ({ setCity, cities }) => {
+  const searchByCity = (cityName) => {
+    setCity(cityName);
+    // let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=89cc8b2a01d33c3c6d58723691b762ac&units=metric`;
+    // let response = await fetch(url);
+    // let data = await res.json();
+  };
   return (
     <div>
       <Button variant="light">Current Location</Button>{" "}
-      {cities.map((item) => (
-        <Button variant="light">{item}</Button>
+      {cities.map((item, index) => (
+        <Button variant="light" key={index} onClick={() => setCity(item)}>
+          {item}
+        </Button>
       ))}
     </div>
   );
