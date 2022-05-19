0d985210186e7bc22e525b62d72fbdc192ada418
leaisrevolution <leaisrevolution@gmail.com>
2022-05-20 00:30:56 +0900

    todo list 컴포넌트 생성

--- codingnoona/react/todolist/src/component/TodoBoard.js
+++ codingnoona/react/todolist/src/component/TodoBoard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 const TodoBoard = () => {
   return (
     <div>
       <h1>Todo List</h1>
+      <TodoItem />
     </div>
   );
 };
