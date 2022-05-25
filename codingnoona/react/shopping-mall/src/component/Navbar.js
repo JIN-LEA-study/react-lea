397a20333e0bec716d082bdaa30640945986237d
leaisrevolution <leaisrevolution@gmail.com>
2022-05-25 18:51:41 +0900

    [#15] style : add Naverbar

--- codingnoona/react/shopping-mall/src/component/Navbar.js
+++ codingnoona/react/shopping-mall/src/component/Navbar.js
@@ -1,16 +1,44 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const menuList = [
+    "여성",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
   return (
     <div>
       <div>
-        <FontAwesomeIcon icon={faUser} />
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="nav-seciton">
+        <img
+          width="70px"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/800px-H%26M-Logo.svg.png"
+          alt=""
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div>
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 };
