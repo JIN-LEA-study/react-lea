d069f7f12f1e2d0884ae8731cdff44106e9b04fa
leaisrevolution <leaisrevolution@gmail.com>
2022-05-25 21:21:10 +0900

    [#19] feat : data, img props

--- codingnoona/react/shopping-mall/src/component/ProductCard.js
+++ codingnoona/react/shopping-mall/src/component/ProductCard.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
     <div>
-      <img
-        src="https://lp2.hm.com/hmgoepprod?set=source[/99/a8/99a84ac1a8c7f94726b0636079528b6b42f34d2f.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]"
-        alt=""
-      />
+      <img src={item?.img} alt="" />
       <div>Conscious choice</div>
-      <div>벨티트 트윌 코트</div>
-      <div>99900</div>
+      <div>{item?.title}</div>
+      <div>{item?.price}</div>
       <div>신제품</div>
     </div>
   );
