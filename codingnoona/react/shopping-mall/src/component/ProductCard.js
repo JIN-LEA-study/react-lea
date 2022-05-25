d33415d36bb632891039b771cfebcd1d6f3ab3eb
leaisrevolution <leaisrevolution@gmail.com>
2022-05-25 21:25:41 +0900

    [#19] edit : 주석 추가

--- codingnoona/react/shopping-mall/src/component/ProductCard.js
+++ codingnoona/react/shopping-mall/src/component/ProductCard.js
@@ -4,10 +4,10 @@ const ProductCard = ({ item }) => {
   return (
     <div>
       <img src={item?.img} alt="" />
-      <div>Conscious choice</div>
+      <div>{item?.choice === true ? "Conscous choice" : ""}</div>
       <div>{item?.title}</div>
       <div>{item?.price}</div>
-      <div>신제품</div>
+      <div>{item?.new === true ? "신제품" : ""}</div>
     </div>
   );
 };
