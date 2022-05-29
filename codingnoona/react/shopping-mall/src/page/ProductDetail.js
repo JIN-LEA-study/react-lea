d79d833670ef712dfd5cd3e06f9174d6116b6028
leaisrevolution <leaisrevolution@gmail.com>
2022-05-29 23:18:03 +0900

    상품 디테일 페이지 만들기

--- codingnoona/react/shopping-mall/src/page/ProductDetail.js
+++ codingnoona/react/shopping-mall/src/page/ProductDetail.js
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `http://localhost:3000/product/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+  return (
+    <Container>
+      <Row>
+        <Col className="product-img">
+          <img src="{product?.img}" alt="" />
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>{product?.price}</div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductDetail;
