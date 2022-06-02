c8aedacf956ae9b0e1bb1f1d883288c4a56d83db
lea <leaisrevolution@gmail.com>
2022-06-03 01:58:20 +0900

    [study] react-router-dom

--- reactProject/mern-stack/client/src/index.js
+++ reactProject/mern-stack/client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import A from "./component/A";
+import B from "./component/B";
+import C from "./component/C";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <App />
+        <Route path="/A" element={<A />} />
+        <Route path="/B" element={<B />} />
+        <Route path="/C" element={<C />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
