a84d33cfbfa02f4107c4b4ff9c65ef50fcb8d36d
leaisrevolution <leaisrevolution@gmail.com>
2022-06-02 17:59:51 +0900

    [style] pink button

--- reactProject/q-project/src/App.js
+++ reactProject/q-project/src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { PinkButton } from "./components/PinkButton";
+import { next } from "./store/modules/score";
+import styled from "styled-components";
+
 function App() {
-  return <div></div>;
+  const page = useSelector((state) => state.score.page);
+  const dispatch = useDispatch();
+  return (
+    <>
+      {page === 0 && (
+        <main>
+          <img src="" alt="" />
+          <h1>나라별 수도 퀴즈</h1>
+          <h2>진정한 수도 고인물도 100점을 맞기 어렵습니다!</h2>
+          <PinkButton
+            text="테스트 시작"
+            clickEvent={() => {
+              dispatch(next());
+            }}
+          />
+        </main>
+      )}
+    </>
+  );
 }
 
 export default App;
