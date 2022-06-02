fc21f175d7b2ffc15553a2af9585c92bfa7077d7
leaisrevolution <leaisrevolution@gmail.com>
2022-06-02 16:48:45 +0900

    [feat] reducer, store

---
+++ reactProject/q-project/src/store/index.js
@@ -0,0 +1,5 @@
+// root reducer 역할을 해줘야 함
+
+import { combineReducers } from "redux";
+
+export default combineReducers({});
---
+++ reactProject/q-project/src/store/modules/score.js
@@ -0,0 +1,79 @@
+// 퀴즈를 풀고 정답인 경우 -> 점수 획득
+// 오답인 경우 -> 점수 획득 X
+
+// 상태 : 사용자의 현재 점수 (score)
+// 액션 : 정답인지? 아닌지? 판별 -> 정답이면 점수 ++;
+
+// 서브 리듀서, 액션 타입(문자열)
+
+const CHECK_CORRECT = "score/CHECK_CORRECT";
+const NEXT_PAGE = "score/NEXT_PAGE";
+const RESET = "score/RESET";
+
+// 액션 생성 함수
+
+export function check({ quizIndex, answerIndex }) {
+  return {
+    type: CHECK_CORRECT,
+    payload: { quizIndex, answerIndex },
+  };
+}
+
+export function next() {
+  return {
+    type: NEXT_PAGE,
+  };
+}
+
+export function reset() {
+  return {
+    type: RESET,
+  };
+}
+
+// 초기 상태
+
+const initialState = {
+  score: 0,
+  page: 0, // 0: 인트로페이지, 1 ~ quizs.length: 퀴즈페이지, quizs.length+1 : 마지막 페이지
+  quizs: [
+    {
+      q: "대한민국의 수도는?",
+      a: [
+        {
+          text: "서울",
+          isCorrect: true,
+        },
+        {
+          text: "인천",
+          isCorrect: false,
+        },
+        {
+          text: "부산",
+          isCorrect: false,
+        },
+      ],
+    },
+  ],
+};
+
+// 리듀서
+
+export default function score(state = initialState, action) {
+  switch (action.type) {
+    case CHECK_CORRECT:
+      return {
+        ...state,
+        score: state.quizs[action.payload.quizIndex].isCorrect
+          ? state.score + 10
+          : state.score,
+      };
+    case NEXT_PAGE:
+      return {
+        ...state,
+        page: state.page + 1,
+      };
+    default:
+      return state;
+  }
+}
