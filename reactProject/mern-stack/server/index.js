b2345aa67ebabcbcba66616083ff02685d629d94
lea <leaisrevolution@gmail.com>
2022-06-03 13:58:08 +0900

    [feat] then, cathch

--- reactProject/mern-stack/server/index.js
+++ reactProject/mern-stack/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 5000;
 
@@ -8,7 +10,17 @@ const port = 5000;
 app.use(express.static(path.join(__dirname, "../client/build")));
 // 서버 실행
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  mongoose
+    .connect(
+      "mongodb+srv://leaisrevolution:gpdnjsdl27@cluster0.v7mku.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log(`Example app listening on port ${port}`);
+      console.log("connection MongoDB...");
+    })
+    .catch((err) => {
+      console.log(`${err}`);
+    });
 });
 
 app.get("/", (req, res) => {
