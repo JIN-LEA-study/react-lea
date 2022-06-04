ffeea6842612b30ed66ec90e24c1d048b82f5e31
lea <leaisrevolution@gmail.com>
2022-06-04 18:51:06 +0900

    [feat] redux store

--- reactProject/lea-music-player/src/store/mvPlayerList.js
+++ reactProject/lea-music-player/src/store/mvPlayerList.js
@@ -22,3 +22,80 @@ const playList = [
     avi: mv3,
   },
 ];
+
+const initialState = {
+  playList,
+  currentMusicId: playList[0].id,
+  currentIndex: 0,
+  playing: false,
+};
+
+const PLAY_MUSIC = "musicPlayer/PLAY_MUSIC";
+const STOP_MUSIC = "musicPlayer/STOP_MUSIC";
+const NEXT_MUSIC = "musicPlayer/NEXT_MUSIC";
+const PREV_MUSIC = "musicPlayer/PREV_MUSIC";
+const SET_CURRENT_INDEX = "musicPlayer/SET_CURRENT_INDEX";
+
+export const playMusic = () => ({ type: PLAY_MUSIC });
+export const stopMusic = () => ({ type: STOP_MUSIC });
+export const nextMusic = () => ({ type: NEXT_MUSIC });
+export const prevMusic = () => ({ type: PREV_MUSIC });
+export const setCurrentIndex = (index) => ({ type: SET_CURRENT_INDEX, index });
+
+const getRandomNum = (arr, excludeNum) => {
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber] === excludeNum
+    ? getRandomNum(arr, excludeNum)
+    : arr[randomNumber];
+};
+
+export default function mvPlayerReducer(state = initialState, action) {
+  switch (action.type) {
+    case PLAY_MUSIC:
+      return {
+        ...state,
+        playing: true,
+      };
+    case STOP_MUSIC:
+      return {
+        ...state,
+        playing: false,
+      };
+    case SET_CURRENT_INDEX:
+      return {
+        ...state,
+        currentIndex: action.index,
+        currentMusicId: state.playList[action.index].id,
+      };
+    case NEXT_MUSIC:
+      const nextIndex =
+        state.repeat === "SHUFFLE"
+          ? getRandomNum(
+              Array.from(Array(playList.length).keys()),
+              state.currentIndex
+            )
+          : (state.currentIndex + 1) % state.playList.length;
+      return {
+        ...state,
+        currentIndex: nextIndex,
+        currentMusicId: state.playList[nextIndex].id,
+      };
+    case PREV_MUSIC:
+      const prevIndex =
+        state.repeat === "SHUFFLE"
+          ? getRandomNum(
+              Array.from(Array(playList.length).keys()),
+              state.currentIndex
+            )
+          : (state.currentIndex - 1 + state.playList.length) %
+            state.playList.length;
+      return {
+        ...state,
+        currentIndex: prevIndex,
+        currentMusicId: state.playList[prevIndex].id,
+      };
+
+    default:
+      return state;
+  }
+}
