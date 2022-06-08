c3fe933fdab32678cf3aef718b807dc561244bf4
lea <leaisrevolution@gmail.com>
2022-06-09 00:41:38 +0900

    [feat] Router

--- codingnoona/react/netflix/src/App.js
+++ codingnoona/react/netflix/src/App.js
@@ -1,7 +1,17 @@
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </div>
   );
 }
 
