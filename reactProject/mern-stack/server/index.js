fbfe209f09b3dbe53d62aea57784a508791dbdcf
lea <leaisrevolution@gmail.com>
2022-06-09 21:33:40 +0900

    [fix] name, postNum err

--- reactProject/mern-stack/server/index.js
+++ reactProject/mern-stack/server/index.js
@@ -11,9 +11,9 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//
-const { Post } = require("./Model/Post.js");
-const { Counter } = require("./Model/Counter.js");
+// 라우터 경로
+app.use("/api/post", require("./Router/post"));
+
 // 서버 실행
 app.listen(port, () => {
   mongoose
@@ -35,74 +35,4 @@ app.get("/", (req, res) => {
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-app.post("/api/post/submit", (req, res) => {
-  let temp = req.body;
-  Counter.findOne({ name: "counter" })
-    .exec()
-    .then((counter) => {
-      temp.postNum = counter.postNum; //counter 안에 있는 num을 추가
-      const CommunityPost = new Post(temp);
-      CommunityPost.save().then(() => {
-        Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(
-          () => {
-            res.status(200).json({ success: true });
-          }
-        );
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-app.post("/api/post/list", (req, res) => {
-  Post.find()
-    .exec()
-    .then((doc) => {
-      res.status(200).json({ success: true, postList: doc });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-app.post("/api/post/detail", (req, res) => {
-  Post.findOne({ postNum: Number(req.body.postNum) })
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      res.status(200).json({ success: true, post: doc });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-app.post("/api/post/edit", (req, res) => {
-  let temp = {
-    title: req.body.title,
-    content: req.body.content,
-  };
-  Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-app.post("/api/post/delete", (req, res) => {
-  Post.deleteOne({ postNum: Number(req.body.postNum) })
-    .exec()
-    .then(() => {
-      console.log();
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
 });
