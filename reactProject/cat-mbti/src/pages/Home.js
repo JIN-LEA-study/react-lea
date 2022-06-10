493bef52fd567a7b3ac2540bc7fea916884aca3f
lea <leaisrevolution@gmail.com>
2022-06-10 19:10:44 +0900

    [feat] question page, add img

--- reactProject/cat-mbti/src/pages/Home.js
+++ reactProject/cat-mbti/src/pages/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Summer from "../assets/KakaoTalk_Photo_2021-09-13-15-30-10 002.jpg";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const handleClickButton = () => {
+    //페이지 이동
+    navigate("/question");
+  };
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
@@ -18,7 +24,9 @@ function Home() {
           />
         </LogoImg>
         <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기</Desc>
-        <Button>Go! Test</Button>
+        <Button variant="light" onClick={handleClickButton}>
+          Go! Test
+        </Button>
       </Contents>
     </Wrapper>
   );
@@ -35,6 +43,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "yg-jalnan";
 `;
 
 const Contents = styled.div`
@@ -42,11 +51,13 @@ const Contents = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  font-family: "S-CoreDream-3Light";
 `;
 
 const Title = styled.div`
   font-size: 30pt;
   margin-top: 40px;
+  font-family: "S-CoreDream-3Light";
 `;
 
 const LogoImg = styled.div`
@@ -56,6 +67,7 @@ const LogoImg = styled.div`
 const Desc = styled.div`
   font-size: 20pt;
   margin-top: 20px;
+  font-family: "S-CoreDream-3Light";
 `;
 
 export default Home;
--- reactProject/cat-mbti/src/pages/Question.js
+++ reactProject/cat-mbti/src/pages/Question.js
@@ -1,7 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+import { ProgressBar, Button } from "react-bootstrap";
+import { questionData } from "../assets/data/questionData";
 
 function Question() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <ProgressBar
+        striped
+        variant="danger"
+        now={80}
+        style={{ marginTop: "20px" }}
+      />
+      <Title>{questionData[0].title}</Title>
+      <ButtonSection>
+        <Button
+          style={{
+            width: "40%",
+            minHeight: "200px",
+            fontSize: "15pt",
+            marginRight: "20px",
+          }}
+        >
+          {questionData[0].answerA}
+        </Button>
+        <Button
+          style={{
+            width: "40%",
+            minHeight: "200px",
+            fontSize: "15pt",
+          }}
+        >
+          {questionData[0].answerB}
+        </Button>
+      </ButtonSection>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  /* background-color: pink; */
+  height: 100vh;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+  text-align: center;
+  font-family: "S-CoreDream-3Light";
+`;
+
+const ButtonSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-family: "S-CoreDream-3Light";
+`;
 
 export default Question;
