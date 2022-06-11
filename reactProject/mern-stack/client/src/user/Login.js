da1f2ede466567ad800ae873fe66b328b7cde762
lea <leaisrevolution@gmail.com>
2022-06-11 16:24:02 +0900

    [feat] login

--- reactProject/mern-stack/client/src/user/Login.js
+++ reactProject/mern-stack/client/src/user/Login.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 import { LoginDiv } from "../Style/UserCss";
 import { useNavigate } from "react-router-dom";
-
+import firebase from "../component/firebase";
 function Login() {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const navigate = useNavigate();
 
+  const SingInFunc = async (e) => {
+    e.preventDefault();
+    if (!(email && pw)) {
+      return alert("모든 값을 채워주세요.");
+    }
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, pw);
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === "auth/user-not-found") {
+        setErrorMsg("존재하지 않는 이메일입니다.");
+      } else if (error.code === "auth/wrong-password") {
+        setErrorMsg("비밀번호가 일치하지 않습니다.");
+      } else {
+        setErrorMsg("로그인이 실패하였습니다.");
+      }
+    }
+  };
   return (
     <LoginDiv>
       <form action="">
@@ -14,6 +34,7 @@ function Login() {
         <input
           type="email"
           value={email}
+          required
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
         <label>Password</label>
@@ -22,7 +43,8 @@ function Login() {
           value={pw}
           onChange={(e) => setPw(e.currentTarget.value)}
         />
-        <button>Login</button>
+        {errorMsg !== "" && <p>{errorMsg}</p>}
+        <button onClick={(e) => SingInFunc(e)}>LogIn</button>
         <button
           onClick={(e) => {
             e.preventDefault();
