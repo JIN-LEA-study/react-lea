1084a7bf638185516b02e50ee72a6d9abda56e11
lea <leaisrevolution@gmail.com>
2022-06-11 22:57:28 +0900

    [feat] logout

--- reactProject/mern-stack/client/src/component/Heading.js
+++ reactProject/mern-stack/client/src/component/Heading.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import firebase from "../component/firebase";
 
 function Heading() {
+  const navigate = useNavigate();
+
+  // user 정보를 받아옴
+  const user = useSelector((state) => state.user);
+
+  //logout
+  const LogoutHandler = () => {
+    navigate("/");
+    firebase.auth().signOut();
+  };
+
   return (
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">LEA's Cummunity</Navbar.Brand>
+          <Navbar.Brand href="/">LEA's Cummunity</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -31,17 +44,27 @@ function Heading() {
               >
                 upload
               </Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            {user.accessToken ? (
+              <Navbar.Text
+                style={{ cursor: "pointer" }}
+                onClick={() => LogoutHandler()}
+              >
+                Logout
+              </Navbar.Text>
+            ) : (
               <Link
                 to="/login"
                 style={{
                   color: "black",
                   textDecoration: "none",
-                  marginRight: "10px",
                 }}
               >
                 login
               </Link>
-            </Nav>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
