df2e18423e2388121cc6027cc023e7def8632711
lea <leaisrevolution@gmail.com>
2022-06-12 17:58:53 +0900

    [feat] reducer setting

--- reactProject/diary/src/App.js
+++ reactProject/diary/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useReducer, useRef } from "react";
 
 //router
 import Home from "./pages/Home";
@@ -11,7 +12,67 @@ import MyHeader from "./components/MyHeader";
 
 //component
 
+// reducer
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const newItem = {};
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  // Create
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+  // Remove
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+  // Edit
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: { id: targetId, date: new Date(date).getTime(), content, emotion },
+    });
+  };
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
