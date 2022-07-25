cded1d280c581c917faa1bf06f5048cd43bd4629
leaisrevolution <leaisrevolution@gmail.com>
2022-07-25 21:31:05 +0900

    [add] 여러개의 input 상태 관리하기

--- zerobase/mordenreact/src/InputSample.js
+++ zerobase/mordenreact/src/InputSample.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 
 const InputSample = () => {
-  const [text, setText] = useState("");
-
+  const [inputs, setInputs] = useState({ name: "", nickname: "" });
+  const { name, nickname } = inputs; //비구조할당
   const onChange = (e) => {
-    setText(e.target.value);
+    const { name, value } = e.target;
+    //리액트에서 객체를 다루를 때는 복사를 해야한다.
+    setInputs({
+      ...inputs, //기존의 상태 복사
+      [name]: value, //불변성을 지켜줘야 리액트가 업데이트 상태를 인지한다.
+    });
+  };
+  const onReset = () => {
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
 
-  const onReset = () => {
-    setText("");
-  };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
-        {text}
+        {name} {nickname}
       </div>
     </div>
   );
