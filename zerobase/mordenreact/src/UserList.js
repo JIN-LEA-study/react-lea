de01aa1b732f772016753ee062490735d9d02e7f
leaisrevolution <leaisrevolution@gmail.com>
2022-07-25 21:52:12 +0900

    [add] 배열 렌더링하기 3

--- zerobase/mordenreact/src/UserList.js
+++ zerobase/mordenreact/src/UserList.js
@@ -31,6 +31,7 @@ function UserList() {
     <div>
       {users.map((user, index) => (
         // key props은 각 원소마다 고유값을 주는 것, 리렌더링 성능 최적화를 시켜줌
+        // id가 없을 경우엔 index를 key값에 넣어주는 것이 원칙 (성능적으로 좋아지지 않음)
         <User user={user} key={user.id} />
       ))}
     </div>
