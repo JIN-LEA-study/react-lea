bffb8154ac09cf248f3e0f586adcbfa5d7de7e44
leaisrevolution <leaisrevolution@gmail.com>
2022-07-29 19:09:29 +0900

    [add] Sstup

--- nomadcoder/redux/src/routes/Home.js
+++ nomadcoder/redux/src/routes/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+    setText("");
+  }
+  return (
+    <div>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
