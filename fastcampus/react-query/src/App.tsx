73c0230b22bc6256dabcdb55af50c0a8f0a2122d
leaisrevolution <leaisrevolution@gmail.com>
2022-10-09 17:11:50 +0900

    [Add] useParams

--- fastcampus/react-query/src/App.tsx
+++ fastcampus/react-query/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import DetailPage from "./pages/DetailPage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />}></Route>
+        <Route path="/:id" element={<DetailPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
