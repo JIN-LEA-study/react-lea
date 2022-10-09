703c84f15c1c3be31ed08adc7548409992899121
leaisrevolution <leaisrevolution@gmail.com>
2022-10-09 16:33:04 +0900

    [Add] React Query

---
+++ fastcampus/pokedex/src/components/About.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "@emotion/styled/macro";
+
+const Base = styled.div``;
+const FlavorText = styled.p``;
+const TypeWrapper = styled.div``;
+const TypeList = styled.div``;
+const TypeImg = styled.img``;
+const TypeLabel = styled.span``;
+const TypeWrapper = styled.div``;
+const Img = styled.img``;
+const InfoContainerWrapper = styled.div``;
+const InfoContainer = styled.div``;
+const Title = styled.h4``;
+const InfoItem = styled.div``;
+const InfoItemLabel = styled.span``;
+const InfoItemValue = styled.span``;
+
+const About: React.FC = () => {
+  return (
+    <Base>
+      <FlavorText>
+        <TypeWrapper></TypeWrapper>
+      </FlavorText>
+    </Base>
+  );
+};
+
+export default About;
