5a7123a2f1dcf3bfb64b470aefb4ea33013017d4
leaisrevolution <leaisrevolution@gmail.com>
2022-10-12 23:39:09 +0900

    [Add] Recoil selector

--- nomadcoder/trelloclone/src/App.tsx
+++ nomadcoder/trelloclone/src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState, hourSelector } from "./atoms";
+
+/**
+ * +"1" -> 1 앞에 +를 붙여주면 string 타입을 number 타입으로 바꿔준다.
+ */
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
   return (
     <div>
-      <input type="number" placeholder="Minutes" />
-      <input type="number" placeholder="Hours" />
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
     </div>
   );
 }
